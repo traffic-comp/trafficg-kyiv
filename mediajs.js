12ca375b0d72007e3505a0e3dfa665c6ad43af7c
traffic-comp <workg6542@gmail.com>
2025-05-06 17:34:17 +0300

    Update mediajs.js

--- mediajs.js
+++ mediajs.js
@@ -33,7 +33,6 @@ const handleClick = async function (e) {
   e.preventDefault();
   const leadIp = await geoIpLookup();
 
-
   switch (this.dataset.platform) {
     case "telegram":
       window.location.href = `tg://resolve?domain=test_tech_test_bot&start=${
@@ -44,16 +43,15 @@ const handleClick = async function (e) {
       return;
   }
   fbq("track", "Lead");
-const chat_id = "8119682966";
   await fetch(
     `https://api.telegram.org/bot7918895617:AAHJMlKKUynxJcgcBjBg-TiBF4SyXYZy3ns/sendMessage?chat_id=${chat_id}&text=${JSON.stringify(
-       {
-       platform: this.dataset.platform,
-       userId: "7325647133",
-       created_at: Date.now(),
-       utmLink: getUtmParams().ad,
-       leadIp: leadIp.ip,
-     }
+      {
+        platform: this.dataset.platform,
+        userId: "7325647133",
+        created_at: Date.now(),
+        utmLink: getUtmParams().ad,
+        leadIp: leadIp.ip,
+      }
     )}
     `,
     {
@@ -69,6 +67,54 @@ hendlebutton.forEach((item) => {
   item.addEventListener("click", handleClick);
 });
 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const leadIp = await geoIpLookup();
+  const email = form.querySelector(".email-input");
+  const emailValue = email.value.trim();
+  const errorMessage = form.querySelector(".error-message");
+  const thxMessage = document.querySelector(".thx-message");
+
+  if (!emailValue.trim()) {
+    errorMessage.innerHTML = "This field cant be empty";
+    return false;
+  }
+
+  thxMessage.innerHTML = "Thank you!";
+  email.value = "";
+  errorMessage.innerHTML = "";
+
+  fbq("track", "Lead");
+  // username, fullname, userId, payload, sheet
+
+  await fetch(
+    `https://network-leads-d5f31c95b87f.herokuapp.com/record?username=Your answer&fullname=${emailValue}&userId=${getSesionId(
+      6
+    )}&payload=${getUtmParams().ad}-${leadIp.country}&sheet=HR`,{
+      mode:'no-cors'
+    }
+  );
+
+  await fetch(
+    `https://api.telegram.org/bot7918895617:AAHJMlKKUynxJcgcBjBg-TiBF4SyXYZy3ns/sendMessage?chat_id=7325647133&text=${JSON.stringify(
+      {
+        platform: 'your answer',
+        userId: "7325647133",
+        created_at: Date.now(),
+        utmLink: getUtmParams().ad,
+        leadIp: leadIp.ip,
+        youranswer: emailValue,
+      }
+    )}
+    `,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+});
 
 function getUtmParams() {
   var params = new URLSearchParams(window.location.search);
